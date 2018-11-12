3d859f8f20f219f8a963257da5238c4cddf0ba34
Brian Pennington <srsrec@gmail.com>
2018-11-12 16:35:53 -0600

    finished BinarySearchTree, SEarch function to test

---
+++ DataStructuresCode/BinaryTree/BinarySearchTree.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresCode.BinaryTree
+{
+    public class BinarySearchTree
+    {
+        public Node root;
+
+
+
+       
+
+        public void Add(int numberAdded)
+        {
+            Node nodeToAdd = new Node(numberAdded);
+
+            if (root == null)
+            {
+                root = nodeToAdd;
+                return;
+            }
+
+            Node currentNode = root;
+
+            bool isFinished = false;
+            while(isFinished == false)
+            {
+                if (nodeToAdd.data < currentNode.data)
+                {
+                    if (currentNode.leftChild == null)
+                    {
+                        currentNode.leftChild = nodeToAdd;
+                        isFinished = true;
+                        return;
+                    }
+                    else
+                    {
+                        currentNode = currentNode.leftChild;
+                    }
+                    //  root.leftChild = currentNode;
+                }
+                else if (nodeToAdd.data > currentNode.data)
+                {
+                    if (currentNode.rightChild == null)
+                    {
+                        currentNode.rightChild = nodeToAdd;
+                        isFinished = true;
+                        return;
+                    }
+                    else
+                    {
+                        currentNode = currentNode.rightChild;
+                    }
+                }
+                else
+                {
+                    // numbers are equal, cannot create duplicates
+                    isFinished = true;
+                    return;
+                }
+            }
+
+
+            
+        }
+        public int numToSearch;
+        public void Search(int numToSearch)
+        {
+            Node currentNode = root;
+
+            bool isFinished = false;
+            while (isFinished == false)
+            {
+                if (numToSearch.Equals(currentNode))
+                {
+                    // found number (root)
+                    isFinished = true;
+                    return;
+                }
+                else if (!numToSearch.Equals(currentNode.leftChild))
+                {
+                    if (true)
+                    {
+                        currentNode = currentNode.leftChild;
+                        numToSearch.Equals(currentNode.leftChild);
+                        if (currentNode.leftChild == null)
+                        {
+                            // number doesn't exist in tree
+                            isFinished = true;
+                        }
+                    }
+                    else
+                    {
+                        // found number
+                        isFinished = true;
+                        return;
+                    }
+                }
+                else if (!numToSearch.Equals(currentNode.rightChild))
+                {
+                    if (true)
+                    {
+                        currentNode = currentNode.rightChild;
+                        numToSearch.Equals(currentNode.rightChild);
+                        if (currentNode.rightChild == null)
+                        {
+                            // number doesn't exist in tree
+                            isFinished = true;
+                        }
+                    }
+                    else
+                    {
+                        // found number
+                        isFinished = true;
+                        return;
+                    }
+                }
+            }
+        }
+
+    }
+}
--- DataStructuresCode/Program.cs
+++ DataStructuresCode/Program.cs
@@ -12,6 +12,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataStructuresCode.BinaryTree;
 
 namespace DataStructuresCode
 {
@@ -19,80 +20,14 @@ namespace DataStructuresCode
     {
         static void Main(string[] args)
         {
-            //List Example
-            //List<Customer> customers = new List<Customer>();
-            //Customer mike = new Customer() { Name = "Mike", Age = 29 };
-            //customers.Add(mike);
+            BinarySearchTree binaryTree = new BinarySearchTree();
+            Random r = new Random();
 
-            //Linked List Example
-            //Console.WriteLine("Linked List");
-            //SingleLinkedList singleLinkedList = new SingleLinkedList();
-            //singleLinkedList.CreateList();
-            //singleLinkedList.Menu();
-
-
-            //Sorted List Example
-            //Console.WriteLine("Sorted List");
-            //EmployeeTracker employeeTracker = new EmployeeTracker();
-            //employeeTracker.DisplayEmployeesNameAge();
-
-            //Observable Collection Example
-            //Console.WriteLine("Observable Collection");
-            //MovieTracker movieTracker = new MovieTracker();
-            //movieTracker.GetFastFuriousFilmCollection();
-
-            //Dictionary
-            //Console.WriteLine("Dictionary");
-            //DictionaryExamples dictionaryExamples = new DictionaryExamples();
-            //dictionaryExamples.DisplayAnimalCategorization();
-            //dictionaryExamples.DisplayMonthSeason();
-            //dictionaryExamples.DisplayEmployeeIdName();
-
-            //HashSet
-            //Console.WriteLine("Hash Set");
-            //CountriesTracker countriesTracker = new CountriesTracker();
-            //countriesTracker.CheckHasCountries();
-
-            //Stack
-            //Console.WriteLine("Stack");
-            //StackExample stack = new StackExample();
-            //stack.PushOnToStack();
-
-            //Queue
-            Console.WriteLine("Queue");
-            QueueExample queue = new QueueExample();
-            queue.EnqueueOnQueue();
-
-            ////Binary Tree
-            ////ASCII characters table: https://www.dotnetperls.com/ascii-table
-            Console.WriteLine("Binary Tree");
-            DataStructuresCode.BinaryTree.BinaryTree binaryTree = new DataStructuresCode.BinaryTree.BinaryTree();
-            binaryTree.CreateTree();
-            binaryTree.Display();
-            Console.WriteLine();
-
-            //Pre-order starts at root and works its way down left side of tree
-            Console.WriteLine("Pre-order: ");
-            binaryTree.PreOrder();
-            Console.WriteLine("");
-
-            //In-order starts at the farthest-left node and works its way to the farthest-right
-            Console.WriteLine("In-order: ");
-            binaryTree.InOrder();
-            Console.WriteLine();
-
-            //Post-order starts with the farthest-left tree and works its way to the root tree-by-tree
-            Console.WriteLine("Post-order: ");
-            binaryTree.PostOrder();
-            Console.WriteLine();
-
-            //Level-order works its way node-to-node by level
-            Console.WriteLine("Level order: ");
-            binaryTree.LevelOrder();
-            Console.WriteLine();
-
-            Console.WriteLine($"Height of tree is {binaryTree.Height()}");
-
+            for(int i = 0; i < 10; i++)
+            {
+                binaryTree.Add(r.Next(1, 101));
+            }
+            binaryTree.Search(55);
         }
     }
 }
